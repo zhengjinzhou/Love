a56939f8c48e9d70b0ad438a8741bc17385c4715
zhengjinzhou <1457594845@qq.com>
2017-12-26 16:47:43 +0800

    增加抽奖界面，待完善

--- app/src/main/java/zhou/com/snail/ui/activity/LotteryActivity.java
+++ app/src/main/java/zhou/com/snail/ui/activity/LotteryActivity.java
@@ -2,11 +2,17 @@ package zhou.com.snail.ui.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
+import butterknife.BindView;
+import butterknife.OnClick;
 import zhou.com.snail.R;
 import zhou.com.snail.base.BaseActivity;
 
 public class LotteryActivity extends BaseActivity {
+
+    @BindView(R.id.tv_head) TextView tv_head;
 
     @Override
     protected int getLayout() {
@@ -15,7 +21,16 @@ public class LotteryActivity extends BaseActivity {
 
     @Override
     protected void init() {
+        tv_head.setText("抽奖活动");
+    }
 
+    @OnClick({R.id.back}) void onClick(View view){
+        switch (view.getId()){
+            case R.id.back:
+                finish();
+                break;
+        }
     }
 
 }
+
