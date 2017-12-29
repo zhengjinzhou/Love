3e406359af31293ba9229532d11d5d91417c10a4
zhengjinzhou <1457594845@qq.com>
2017-12-29 14:17:14 +0800

    修改获取时间工具类----获取24小时制

--- app/src/main/java/zhou/com/snail/util/CurrentTimeUtil.java
+++ app/src/main/java/zhou/com/snail/util/CurrentTimeUtil.java
@@ -5,12 +5,16 @@ package zhou.com.snail.util;
  */
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
-
+//改为24小时制
 public class CurrentTimeUtil {
     public static String nowTime(){
-        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        Date date = new Date(System.currentTimeMillis());
-        return format.format(date);
+        Calendar now;
+        SimpleDateFormat fmt;
+
+        now = Calendar.getInstance();
+        fmt = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        return fmt.format(now.getTime());
     }
 }
